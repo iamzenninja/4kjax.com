d3751d121bed3e4b150729bac1c72727f840d456
Sean Michael Harvell <sean.harvell@gmail.com>
2026-08-14 13:46:30 -0400

    update

--- deploy.bat
+++ deploy.bat
@@ -1,4 +1,5 @@
 @echo off
+aws configure get region
 aws s3 sync ./src s3://4kjax-com-prod-use1 --delete --region us-east-1
 aws cloudfront create-invalidation --distribution-id E2MUFZM3OM0PIO --paths "/*"
 echo "make sure no errors"
