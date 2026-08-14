7cd84889f1373e502540819abe98b9af0bac857a
Sean Michael Harvell <sean.harvell@gmail.com>
2026-08-14 10:34:04 -0400

    update

--- deploy.bat
+++ deploy.bat
@@ -2,5 +2,8 @@
 
 aws s3 sync ./src s3://4kjax.com --delete && aws cloudfront create-invalidation --distribution-id E2MUFZM3OM0PIO --paths "/*"
 aws cloudfront create-invalidation --distribution-id E2MUFZM3OM0PIO --paths "/*"
-
+echo "make sure no errors"
 pause
+git add -A
+git commit -m "update"
+git push
