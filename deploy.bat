d06f9e7e04f3ce28932fa9e58c861904ccb3f99b
Sean Michael Harvell <sean.harvell@gmail.com>
2026-08-14 11:25:41 -0400

    update

--- deploy.bat
+++ deploy.bat
@@ -1,6 +1,5 @@
 @echo off
-
-aws s3 sync ./src s3://4kjax.com --delete && aws cloudfront create-invalidation --distribution-id E2MUFZM3OM0PIO --paths "/*"
+aws s3 sync ./src s3://4kjax-com-prod-use1 --delete --region us-east-1
 aws cloudfront create-invalidation --distribution-id E2MUFZM3OM0PIO --paths "/*"
 echo "make sure no errors"
 pause
